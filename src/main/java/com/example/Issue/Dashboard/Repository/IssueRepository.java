ac5ced9d1aa881839debb8bf507a9619926ed810
Pranav <pranav.garg@gdn-commerce.com>
2026-05-06 16:25:58 +0530

    added get api for reading the messages

--- src/main/java/com/example/Issue/Dashboard/Repository/IssueRepository.java
+++ src/main/java/com/example/Issue/Dashboard/Repository/IssueRepository.java
@@ -1,6 +1,9 @@
 package com.example.Issue.Dashboard.Repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface IssueRepository extends MongoRepository<Issue, String> {
+    List<Issue> findByProcessedFalse();
 }
